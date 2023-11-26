33a3da0fcbe563cd37dcc585900a405100b09262
VENGALA SAI KIRAN <123131207+Saikiran174@users.noreply.github.com>
2023-11-25 23:52:17 -0600

    added files

---
+++ src/main/java/com/SaiClasses/DMS/repository/TrainerRepositoty.java
@@ -0,0 +1,11 @@
+package com.SaiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.SaiClasses.DMS.Models.Trainer;
+
+
+
+public interface TrainerRepositoty extends CrudRepository<Trainer, Integer> {
+
+}
